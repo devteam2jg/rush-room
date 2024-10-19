f0ee6fba5c29cf8cbb8e4c10ad3821b9fd45599a
hab56ur9 <hab5bur9@gmail.com>
2024-10-19 12:41:53 +0000

    feat(user): service 로직 추가
    - create 로직추가
    - findById 구현
    - findBySocialId 구현
    - makeUserDto 구현: User Entity 직접 반환은 위험하므로 필요한 정보만 담은 UserDataDto로 변경하기 위한 메서드,
    - UserService에서 반환하는 User 정보는 오직 UserDataDto로만 전달된다.

--- src/domain/users/users.service.ts
+++ src/domain/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '~/src/domain/users/entities/user.entity';
 import { Repository } from 'typeorm';
+import {CreateUserDto, FindByIdDto, FindBySocialIdDto, UserDataDto} from '~/src/domain/users/dto/user.dto';
 
 @Injectable()
 export class UsersService {
@@ -9,4 +10,31 @@ export class UsersService {
     @InjectRepository(User)
     private usersRepository: Repository<User>,
   ) {}
+  
+  async create(createUserDto: CreateUserDto) {
+    const user:User = await this.usersRepository.save(createUserDto);
+    return this.makeUserDto(user);
+  }
+  findById(findByIdDto:FindByIdDto){
+    return this.usersRepository.findOne({where: {id: findByIdDto.id}});
+  }
+
+  async findBySocialId(findBySocialIdDto: FindBySocialIdDto) {
+    const  {socialId, socialType} = findBySocialIdDto;
+    const user:User = await this.usersRepository.findOne({ where: { socialId , socialType} });
+    if(!user){
+      return null;
+    }
+    return this.makeUserDto(user);
+  }
+  makeUserDto(user:User){
+    const userDataDto:UserDataDto = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      socialId: user.socialId,
+      socialType: user.socialType,
+    }
+    return userDataDto;
+  }
 }
