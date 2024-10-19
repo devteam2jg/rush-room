8c601e142a45888490bde47b3ea6568a57325fe4
hab56ur9 <hab5bur9@gmail.com>
2024-10-19 11:55:56 +0000

    feat(users): users entity 추가
    - user entity 추가

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,23 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+    @Column()
+    name: string;
+    @Column()
+    email: string;
+    @Column()
+    password: string;
+    
+    @Column()
+    socialId: string;
+    @Column()
+    socialType: string;
+    // 여기까진 기존의 create-user.dto.ts와 동일합니다.
+
+    @CreateDateColumn()
+    createdAt: Date;
+    @UpdateDateColumn()
+    updatedAt: Date;  
+}
