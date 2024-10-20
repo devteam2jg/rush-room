88987c3fa9a99094ea3901556701540306f9fb7f
hab56ur9 <hab5bur9@gmail.com>
2024-10-20 15:37:16 +0900

    feat(auth): jwt
    - jwt strategy 추가.
    - 기본적인 전략은 cookie에서 accessToken 찾기, validate 리턴값은 JwtPayloadDto이고 이는 req.user로 찾을 수 있음.
    - auth module에 jwt import , configservice에서 환경변수 찾아옴
    - auth module에 passportModule import

---
+++ src/domain/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,29 @@
+import { Strategy, ExtractJwt, StrategyOptions } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtPayloadDto } from '~/src/domain/auth/dto/jwt.dto';
+/* nest 에서 cookie를 다루기위해 express모듈을 사용중임. */
+import { Request } from 'express';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(private readonly configService: ConfigService) {
+        super({
+            /* cookie에 담긴 토큰을 추출 */
+            jwtFromRequest: ExtractJwt.fromExtractors([(req: Request) => {
+              let token = null;
+              if (req && req.cookies) {
+                token = req.cookies['accessToken'];
+              }
+              return token;
+            }]),
+            ignoreExpiration: false,
+            secretOrKey: configService.get<string>('JWT_SECRET'),
+          } as StrategyOptions);
+    }
+
+    async validate(payload: any) {
+        return payload as JwtPayloadDto; //TODO: 이거 검증되는지 확인
+    }
+}
