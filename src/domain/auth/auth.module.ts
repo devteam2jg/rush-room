feca472956f523a5d2529c5620a028b023ce16a7
hab56ur9 <hab5bur9@gmail.com>
2024-10-20 16:10:51 +0900

    feat(auth): service 로직 추가
    - login 함수 추가: UserDataDto형식을 받아서 jwt 토큰을 리턴함.
    - validateUser함수 추가: payload를 받아 해당하는 유저를 찾은 다음 UserDataDto로 리턴함

--- src/domain/auth/auth.module.ts
+++ src/domain/auth/auth.module.ts
@@ -3,10 +3,12 @@ import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from '~/src/domain/users/users.module';
 
 @Module({
   imports: [
     PassportModule,
+    UsersModule,
     JwtModule.registerAsync({
       imports:[ConfigModule],
       useFactory: async (configService: ConfigService) => ({
@@ -16,6 +18,6 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       inject: [ConfigService],
     }),
   ],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
--- src/domain/auth/auth.service.ts
+++ src/domain/auth/auth.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserDataDto } from '~/src/domain/users/dto/user.dto';
+import { UsersService } from '~/src/domain/users/users.service';
+import { JwtPayloadDto } from '~/src/domain/auth/dto/jwt.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly usersService: UsersService,
+        private jwtService: JwtService,
+    ){}
+    async login(user:UserDataDto) {
+        const payload:JwtPayloadDto = { 
+          id: user.id, 
+          name: user.name, 
+          email: user.email, 
+          socialType: user.socialType
+         };
+        return this.jwtService.sign(payload);
+    }
+    async validateUser(payload: JwtPayloadDto) {
+        const user:UserDataDto = await this.usersService.findById({
+          id: payload.id
+        });
+        //TODO: 이부분의 exception 처리는 filter로 빼는게 좋을듯
+        if(!user)
+          throw new UnauthorizedException();
+        return user;
+    }
+}
