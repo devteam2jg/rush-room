996ad81e9c3dada3eb7aad283c5f8a9906cdad0e
smart-cau <1995hyunwoo@naver.com>
2024-10-21 16:33:41 +0900

    feat(error): Add new error code and messages for early event time
    Introduced a new error code `TOO_EARLY_EVENT_TIME` with appropriate message handling in `ServiceException` and `UserErrorMessage`. This ensures events cannot be created too early, adhering to auction policy rules.

--- src/common/exceptions/service.exception.ts
+++ src/common/exceptions/service.exception.ts
@@ -1,6 +1,7 @@
 import {
   ENTITY_NOT_FOUND,
   ErrorCode,
+  TOO_EARLY_EVENT_TIME,
 } from '~/src/common/exceptions/error-code';
 
 export class ServiceException extends Error {
@@ -15,4 +16,10 @@ export class ServiceException extends Error {
 
 export const EntityNotFoundException = (message?: string): ServiceException => {
   return new ServiceException(ENTITY_NOT_FOUND, message);
+};
+
+export const TooEarlyEventTimeException = (
+  message?: string,
+): ServiceException => {
+  return new ServiceException(TOO_EARLY_EVENT_TIME, message);
 };
