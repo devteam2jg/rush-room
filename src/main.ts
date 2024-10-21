ea0e5a3e2a534a3f2833bfc801b4db216889e3e1
smart-cau <1995hyunwoo@naver.com>
2024-10-21 16:31:54 +0900

    feat(global): Enable transformation in global validation pipe
    This allows incoming request data to be automatically transformed to match expected types. Enhancing the global validation pipe helps ensure consistency and reduces potential errors from type mismatches.

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
     credentials: true,
   });
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalFilters(new ServiceExceptionToHttpExceptionFilter());
 
   setupSwagger(app);
