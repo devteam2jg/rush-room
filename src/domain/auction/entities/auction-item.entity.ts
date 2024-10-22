f93e96d742a566c857e4a8ea72ce75f6cbb01dae
smart-cau <1995hyunwoo@naver.com>
2024-10-22 21:58:10 +0900

    feat(auction-item): Add AuctionItem entity to auction domain
    Created an AuctionItem entity to represent items in an auction. This includes various attributes such as title, description, images, start price, last price, and the relationship with users and auctions. This entity will help in managing and associating auction items efficiently.

---
+++ src/domain/auction/entities/auction-item.entity.ts
@@ -0,0 +1,42 @@
+import { CommonEntity } from '~/src/common/entities/common.entity';
+import { Column, Entity, ManyToOne } from 'typeorm';
+import { User } from '~/src/domain/users/entities/user.entity';
+import { Auction } from '~/src/domain/auction/entities/auction.entity';
+
+@Entity('auction-item')
+export class AuctionItem extends CommonEntity {
+  @Column({ type: 'varchar', length: 255 })
+  title: string;
+
+  @Column({ type: 'text' })
+  description: string;
+
+  @Column({ type: 'text', nullable: true })
+  itemImages: string;
+
+  @Column({ type: 'int' })
+  startPrice: number;
+
+  @Column({ type: 'int', nullable: true })
+  lastPrice: number;
+
+  @Column({ type: 'boolean', default: false })
+  isSold: boolean;
+
+  @Column()
+  buyerId: string;
+
+  @ManyToOne(() => User, {
+    onDelete: 'CASCADE',
+    eager: true,
+    createForeignKeyConstraints: false,
+  })
+  user: User;
+
+  @ManyToOne(() => Auction, {
+    onDelete: 'CASCADE',
+    eager: true,
+    createForeignKeyConstraints: false,
+  })
+  auction: Auction;
+}
