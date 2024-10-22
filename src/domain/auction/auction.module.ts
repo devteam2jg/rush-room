c9bcabb4b90578c4a3ad2c7ec716f2220500b35f
smart-cau <1995hyunwoo@naver.com>
2024-10-22 21:59:29 +0900

    feat(auction-item): Add AuctionItem entities and repository to AuctionModule
    Include AuctionItem and AuctionItemRepository for better modularization and functionality. This change ensures that auction items are properly managed within the auction domain.

--- src/domain/auction/auction.module.ts
+++ src/domain/auction/auction.module.ts
@@ -5,10 +5,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Auction } from '~/src/domain/auction/entities/auction.entity';
 import { AuctionRepository } from '~/src/domain/auction/auction.repository';
 import { AuctionManager } from '~/src/domain/auction/auction.manager';
+import { AuctionItem } from '~/src/domain/auction/entities/auction-item.entity';
+import { AuctionItemRepository } from '~/src/domain/auction/auction-item.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Auction])],
+  imports: [TypeOrmModule.forFeature([Auction, AuctionItem])],
   controllers: [AuctionController],
-  providers: [AuctionService, AuctionRepository, AuctionManager],
+  providers: [
+    AuctionService,
+    AuctionRepository,
+    AuctionManager,
+    AuctionItemRepository,
+  ],
 })
 export class AuctionModule {}
