2c73b0b1c49c4edc81b0d9f1e9d5e9acc5885e2f
smart-cau <1995hyunwoo@naver.com>
2024-10-22 23:38:32 +0900

    docs(auction): Add Swagger annotations to AuctionController
    Introduced `ApiOperation` and `ApiResponse` decorators to provide detailed Swagger documentation for each endpoint in the `AuctionController`. These changes will help in generating comprehensive API documentation and improving developer experience.

--- src/domain/auction/auction.controller.ts
+++ src/domain/auction/auction.controller.ts
@@ -18,13 +18,21 @@ import { CreateAuctionResultDto } from '~/src/domain/auction/dto/create-auction-
 import { ReadAuctionDto } from '~/src/domain/auction/dto/read-auction.dto';
 import { CreateAuctionItemDto } from '~/src/domain/auction/dto/create.auction.item.dto';
 import { CreateAuctionItemResultDto } from '~/src/domain/auction/dto/create.auction.item.result.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
-@Controller('auction')
+@ApiTags('Auction apis')
 @UseGuards(JwtAuthGuard)
+@Controller('auction')
 export class AuctionController {
   constructor(private readonly auctionService: AuctionService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create auction' })
+  @ApiResponse({
+    status: 201,
+    description: 'Auction successfully created.',
+    type: CreateAuctionResultDto,
+  })
   create(
     @Body() createAuctionDto: CreateAuctionDto,
     @GetJwtPayload() jwtPayload: JwtPayloadDto,
@@ -33,6 +41,12 @@ export class AuctionController {
   }
 
   @Post(':id/auction-item')
+  @ApiOperation({ summary: 'Create auction item' })
+  @ApiResponse({
+    status: 201,
+    description: 'Auction item successfully created.',
+    type: CreateAuctionItemResultDto,
+  })
   createAuctionItem(
     @Param('id') auctionId: string,
     @Body() createAuctionItemDto: CreateAuctionItemDto,
@@ -46,11 +60,20 @@ export class AuctionController {
   }
 
   @Get()
+  @ApiOperation({ summary: 'NOT_IMPLEMENTED Get all auctions' })
+  @ApiResponse({ status: 200, description: 'Return all auctions.' })
   findAll() {
     return this.auctionService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get auction by ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return auction by ID. served with enlisted auction items',
+    type: ReadAuctionDto,
+  })
+  @ApiResponse({ status: 404, description: 'Auction not found.' })
   findOne(
     @Param('id') id: string,
     @GetJwtPayload() jwtPayload: JwtPayloadDto,
@@ -59,6 +82,12 @@ export class AuctionController {
   }
 
   @Patch(':id')
+  @ApiOperation({
+    summary:
+      'Update auction. 경매 상세정보 수정 뿐만 아니라 경매의 상태(시작, 종료 등) 수정에도 사용할 수 있음',
+  })
+  @ApiResponse({ status: 200, description: 'Auction successfully updated.' })
+  @ApiResponse({ status: 404, description: 'Auction not found.' })
   update(
     @Param('id') id: string,
     @Body() updateAuctionDto: UpdateAuctionDto,
@@ -68,6 +97,9 @@ export class AuctionController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'NOT_IMPLEMENTED Delete auction' })
+  @ApiResponse({ status: 200, description: 'Auction successfully deleted.' })
+  @ApiResponse({ status: 404, description: 'Auction not found.' })
   remove(@Param('id') id: string) {
     return this.auctionService.remove(id);
   }
