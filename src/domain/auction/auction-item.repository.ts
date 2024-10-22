59dc9799782ecf56d290e866b276ec4302d865b0
smart-cau <1995hyunwoo@naver.com>
2024-10-22 23:29:08 +0900

    feat(auction-item): Add method to fetch auction items by auction ID
    Introduced a new method `getAuctionItemsByAuctionId` in `AuctionItemRepository` to fetch auction items using their auction ID. This method queries the database, joins related user data, and returns the results as `ReadAuctionItemDto`.

--- src/domain/auction/auction-item.repository.ts
+++ src/domain/auction/auction-item.repository.ts
@@ -4,6 +4,7 @@ import { AuctionItem } from '~/src/domain/auction/entities/auction-item.entity';
 import { CreateAuctionItemDto } from '~/src/domain/auction/dto/create.auction.item.dto';
 import { JwtPayloadDto } from '~/src/domain/auth/dto/jwt.dto';
 import { CreateAuctionItemResultDto } from '~/src/domain/auction/dto/create.auction.item.result.dto';
+import { ReadAuctionItemDto } from '~/src/domain/auction/dto/read.auction.item.dto';
 
 @Injectable()
 export class AuctionItemRepository extends Repository<AuctionItem> {
@@ -23,5 +24,16 @@ export class AuctionItemRepository extends Repository<AuctionItem> {
     });
     const createdAuctionItem = await this.save(auctionItem);
     return new CreateAuctionItemResultDto(createdAuctionItem);
+  }
+
+  async getAuctionItemsByAuctionId(
+    auctionId: string,
+  ): Promise<ReadAuctionItemDto[]> {
+    const result = await this.createQueryBuilder('auction_item')
+      .leftJoinAndSelect('auction_item.user', 'user')
+      .where('auction_item.auction.id = :auctionId', { auctionId })
+      .select(['auction_item', 'user.id'])
+      .getMany();
+    return result.map((item) => new ReadAuctionItemDto(item, item.user));
   }
 }
