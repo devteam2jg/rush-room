6b3dea930e5fd7f813d275aa2e5ac8ac7f0a5163
YJCMS <sen9563@gmail.com>
2024-10-23 13:15:49 +0900

    fix(webSocket): gateway.ts의 default/updated price 관련 로직 수정

--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -7,7 +7,10 @@ import {
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway({ namespace: '/auction-execute' }) // 특정 namespace를 경매용으로 사용
+@WebSocketGateway({
+  namespace: '/auction-execute',
+  cors: { origin: true, credentials: true },
+}) // 특정 namespace를 경매용으로 사용
 export class AuctionGateway {
   @WebSocketServer()
   server: Server;
@@ -20,8 +23,14 @@ export class AuctionGateway {
     @MessageBody() auctionId: string, // 경매 ID
     @ConnectedSocket() socket: Socket,
   ) {
+    console.log('auction id ======', auctionId);
     socket.join(auctionId); // 해당 경매방에 소켓을 참여시킴
-    const currentBid = this.currentBids[auctionId] || 0; // 설정되지 않은 경우 0으로 초기화
+
+    // defualt 가격
+    if (this.currentBids[auctionId] == null) {
+      this.currentBids[auctionId] = 50000;
+    }
+    const currentBid = this.currentBids[auctionId]; // 설정되지 않은 경우 50000으로 초기화
     // 해당 경매의 현재 최고가를 전송
     socket.emit('current_bid', currentBid);
   }
@@ -29,17 +38,28 @@ export class AuctionGateway {
   // 새로운 가격 제시
   @SubscribeMessage('new_bid')
   handleNewBid(
-    @MessageBody() bidData: { auctionId: string; bidAmount: number },
+    @MessageBody() bidData: { auctionId: string; newCurrentBid: number },
     @ConnectedSocket() socket: Socket,
   ) {
-    const { auctionId, bidAmount } = bidData;
+    const { auctionId, newCurrentBid } = bidData;
+
+    // 콘솔, 데이터 확인
+    console.log('bidData ======', bidData);
+    console.log('newCurrentBid ==== ', newCurrentBid);
+    console.log(
+      'this.currentBids[auctionId] === ',
+      this.currentBids[auctionId],
+    );
+    console.log('condition ==== ', newCurrentBid > this.currentBids[auctionId]);
 
     // 현재 최고가보다 높은 금액만 허용
-    if (bidAmount > (this.currentBids[auctionId] || 0)) {
-      this.currentBids[auctionId] = bidAmount; // 최고가 업데이트
+    if (newCurrentBid > this.currentBids[auctionId]) {
+      this.currentBids[auctionId] = newCurrentBid; // 최고가 업데이트
       // 해당 경매방의 모든 사용자에게 새로운 최고가 알림
-      this.server.to(auctionId).emit('bid_updated', bidAmount);
-      console.log(`Auction ${auctionId} has a new highest bid: ${bidAmount}`);
+      this.server.to(auctionId).emit('bid_updated', newCurrentBid);
+      console.log(
+        `Auction ${auctionId} has a new highest bid: ${newCurrentBid}`,
+      );
     } else {
       socket.emit(
         'bid_error',
