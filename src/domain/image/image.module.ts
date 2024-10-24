d06c299cf22c6803a50ac17ff1f07574de639fd8
hab56ur9 <hab5bur9@gmail.com>
2024-10-24 22:26:15 +0900

    feat(image): image module에 환경 변수 등록

--- src/domain/image/image.module.ts
+++ src/domain/image/image.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { ImageService } from './image.service';
 import { ImageController } from './image.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule],
   controllers: [ImageController],
-  providers: [ImageService],
+  providers: [
+    ImageService,
+    {
+      provide: 'S3_IMAGE_KEY',
+      useFactory: (configService: ConfigService) =>
+        configService.get<string>('S3_IMAGE_KEY'),
+    },
+    {
+      provide: 'S3_IMAGE_SECRET',
+      useFactory: (configService: ConfigService) =>
+        configService.get<string>('S3_IMAGE_SECRET'),
+    },
+    {
+      provide: 'S3_IMAGE_BUCKET',
+      useFactory: (configService: ConfigService) =>
+        configService.get<string>('S3_IMAGE_BUCKET'),
+    },
+  ],
 })
 export class ImageModule {}
--- src/domain/image/image.service.ts
+++ src/domain/image/image.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateImageDto } from './dto/create-image.dto';
 
 @Injectable()
 export class ImageService {
+  constructor(
+    @Inject('S3_IMAGE_KEY') private readonly image_key,
+    @Inject('S3_IMAGE_SECRET') private readonly imaiage_secret,
+  ) {}
   create(createImageDto: CreateImageDto) {
     return 'This action adds a new image';
   }
