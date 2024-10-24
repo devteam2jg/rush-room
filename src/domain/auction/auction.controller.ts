2a25496db522cb563a3c2503671d63b6fcd20f0c
smart-cau <1995hyunwoo@naver.com>
2024-10-24 23:02:20 +0900

    feat(auction): Add pagination support for auctions and auction items
    Refactored the auction controller to include methods for fetching paginated auctions and auction items. Introduced new query parameters for userId and buyerId to filter results, and integrated response decorators for Swagger documentation.

--- src/domain/auction/auction.controller.ts
+++ src/domain/auction/auction.controller.ts
@@ -6,6 +6,7 @@ import {
   Param,
   Patch,
   Post,
+  Query,
   UseGuards,
 } from '@nestjs/common';
 import { AuctionService } from './auction.service';
@@ -18,8 +19,16 @@ import { CreateAuctionResultDto } from '~/src/domain/auction/dto/create-auction-
 import { ReadAuctionDto } from '~/src/domain/auction/dto/read-auction.dto';
 import { CreateAuctionItemDto } from '~/src/domain/auction/dto/auction-item/create.auction.item.dto';
 import { CreateAuctionItemResultDto } from '~/src/domain/auction/dto/auction-item/create.auction.item.result.dto';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ReadAuctionItemDetailDto } from '~/src/domain/auction/dto/auction-item/read.auction.item.detail.dto';
+import { PaginationRequest } from '~/src/common/pagination/pagination.request';
+import { GetPagination } from '~/src/common/pagination/get.pagination.decorator';
+import {
+  ApiPaginationRequest,
+  ApiPaginationResponse,
+} from '~/src/common/pagination/pagination.swagger';
+import { PaginationResponse } from '~/src/common/pagination/pagination.response';
+import { ReadAuctionItemDto } from '~/src/domain/auction/dto/auction-item/read.auction.item.dto';
 
 @ApiTags('Auction apis')
 @UseGuards(JwtAuthGuard)
@@ -61,10 +70,40 @@ export class AuctionController {
   }
 
   @Get()
-  @ApiOperation({ summary: 'NOT_IMPLEMENTED Get all auctions' })
-  @ApiResponse({ status: 200, description: 'Return all auctions.' })
-  findAll() {
-    return this.auctionService.findAll();
+  @ApiOperation({ summary: 'Get paginated auctions' })
+  @ApiResponse({ status: 200, description: 'Return paginated auctions.' })
+  @ApiPaginationRequest()
+  @ApiPaginationResponse(ReadAuctionDto)
+  @ApiQuery({
+    name: 'userId',
+    required: false,
+    type: String,
+    description:
+      '값을 주지 않으면 전체 경매 목록, userId 값을 주면 해당 유저의 경매 목록',
+  })
+  findPaginatedAuctions(
+    @GetPagination() pagination: PaginationRequest,
+    @Query('userId') userId?: string,
+  ): Promise<PaginationResponse<ReadAuctionDto>> {
+    return this.auctionService.getPaginatedAuctions(pagination, userId);
+  }
+
+  @Get('/item')
+  @ApiOperation({ summary: 'Get paginated auction items by buyerId' })
+  @ApiResponse({ status: 200, description: 'Return paginated auction items.' })
+  @ApiPaginationRequest()
+  @ApiPaginationResponse(ReadAuctionItemDto)
+  @ApiQuery({
+    name: 'buyerId',
+    required: true,
+    type: String,
+    description: 'buyerId가 구매한 물품 목록',
+  })
+  findPaginatedAuctionItems(
+    @GetPagination() pagination: PaginationRequest,
+    @Query('buyerId') buyerId: string,
+  ): Promise<PaginationResponse<ReadAuctionItemDto>> {
+    return this.auctionService.getPaginatedAuctionItems(pagination, buyerId);
   }
 
   @Get(':id')
