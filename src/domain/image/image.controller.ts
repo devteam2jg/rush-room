41932ae7580e97f409805399c36418b61296285c
hab56ur9 <hab5bur9@gmail.com>
2024-10-24 22:14:37 +0900

    feat(image): 필요없는 route 제거

--- src/domain/image/image.controller.ts
+++ src/domain/image/image.controller.ts
@@ -1,7 +1,6 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { ImageService } from './image.service';
 import { CreateImageDto } from './dto/create-image.dto';
-import { UpdateImageDto } from './dto/update-image.dto';
 
 @Controller('image')
 export class ImageController {
@@ -12,23 +11,8 @@ export class ImageController {
     return this.imageService.create(createImageDto);
   }
 
-  @Get()
-  findAll() {
-    return this.imageService.findAll();
-  }
-
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.imageService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateImageDto: UpdateImageDto) {
-    return this.imageService.update(+id, updateImageDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.imageService.remove(+id);
   }
 }
--- src/domain/image/image.service.ts
+++ src/domain/image/image.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { CreateImageDto } from './dto/create-image.dto';
-import { UpdateImageDto } from './dto/update-image.dto';
 
 @Injectable()
 export class ImageService {
@@ -8,19 +7,7 @@ export class ImageService {
     return 'This action adds a new image';
   }
 
-  findAll() {
-    return `This action returns all image`;
-  }
-
   findOne(id: number) {
     return `This action returns a #${id} image`;
-  }
-
-  update(id: number, updateImageDto: UpdateImageDto) {
-    return `This action updates a #${id} image`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} image`;
   }
 }
