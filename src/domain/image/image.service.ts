dc70df13a95f46dfde651ce834bc883ec6bb5c4f
hab56ur9 <hab5bur9@gmail.com>
2024-10-24 23:45:14 +0900

    feat(image): upload 함수 구현
    - 기존 create에 존재하던 로직 upload로 이동
    - 매 요청마다 client 새롭게 만들던 로직 처음 service 생성시 1회실행으로 변경

--- src/domain/image/image.service.ts
+++ src/domain/image/image.service.ts
@@ -1,17 +1,36 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { CreateImageDto } from './dto/create-image.dto';
+import { Inject, Injectable, BadRequestException } from '@nestjs/common';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 
 @Injectable()
 export class ImageService {
+  private readonly client;
   constructor(
     @Inject('S3_IMAGE_KEY') private readonly image_key,
-    @Inject('S3_IMAGE_SECRET') private readonly imaiage_secret,
-  ) {}
-  create(createImageDto: CreateImageDto) {
-    return 'This action adds a new image';
+    @Inject('S3_IMAGE_SECRET') private readonly image_secret,
+    @Inject('S3_IMAGE_BUCKET') private readonly image_bucket,
+    @Inject('S3_IMAGE_REGION') private readonly image_region,
+  ) {
+    this.client = new S3Client({
+      region: this.image_region,
+      credentials: {
+        accessKeyId: this.image_key,
+        secretAccessKey: this.image_secret,
+      },
+    });
   }
-  upload(file: Express.Multer.File) {
-    console.log(file);
+
+  async upload(file: Express.Multer.File) {
+    const key = `${Date.now().toString()}-${file.originalname}`;
+    const params = {
+      Key: key,
+      Body: file.buffer,
+      Bucket: this.image_bucket,
+      ContentType: file.mimetype,
+    };
+    const command = new PutObjectCommand(params);
+    const uploadFileS3 = await this.client.send(command);
+    if (uploadFileS3.$metadata.httpStatusCode !== 200)
+      throw new BadRequestException('Failed upload File');
   }
 
   findOne(id: number) {
