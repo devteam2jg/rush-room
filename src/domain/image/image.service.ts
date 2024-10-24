eb904754856278f09cbaa8c51549980aa9f42301
hab56ur9 <hab5bur9@gmail.com>
2024-10-25 00:05:42 +0900

    Refactor(image): 환경 변수 관련 리펙토링
    - 환경변수를 service내에서만 사용할것이라 예상, class내부 변수로 저장.
    - 환경변수 없을시에 예외처리 용 exception 추가

--- src/domain/image/image.service.ts
+++ src/domain/image/image.service.ts
@@ -1,15 +1,22 @@
-import { Inject, Injectable, BadRequestException } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { ConfigService } from '@nestjs/config';
+import { MissingConfigurationsException } from '~/src/common/exceptions/service.exception';
 
 @Injectable()
 export class ImageService {
-  private readonly client;
-  constructor(
-    @Inject('S3_IMAGE_KEY') private readonly image_key,
-    @Inject('S3_IMAGE_SECRET') private readonly image_secret,
-    @Inject('S3_IMAGE_BUCKET') private readonly image_bucket,
-    @Inject('S3_IMAGE_REGION') private readonly image_region,
-  ) {
+  private readonly client: S3Client;
+  private readonly image_key: string;
+  private readonly image_secret: string;
+  private readonly image_bucket: string;
+  private readonly image_region: string;
+
+  constructor(configService: ConfigService) {
+    this.image_bucket = configService.get<string>('S3_IMAGE_BUCKET_NAME');
+    this.image_key = configService.get<string>('S3_IMAGE_ACCESS_KEY');
+    this.image_secret = configService.get<string>('S3_IMAGE_ACCESS_SECRET');
+    this.image_region = configService.get<string>('S3_REGION');
+    this.checkConfigurations();
     this.client = new S3Client({
       region: this.image_region,
       credentials: {
@@ -29,11 +36,22 @@ export class ImageService {
     };
     const command = new PutObjectCommand(params);
     const uploadFileS3 = await this.client.send(command);
+
     if (uploadFileS3.$metadata.httpStatusCode !== 200)
       throw new BadRequestException('Failed upload File');
   }
 
   findOne(id: number) {
     return `This action returns a #${id} image`;
+  }
+
+  private checkConfigurations() {
+    if (
+      !this.image_bucket ||
+      !this.image_key ||
+      !this.image_secret ||
+      !this.image_region
+    )
+      throw MissingConfigurationsException('Missing S3 Configurations');
   }
 }
