0419375231dde9abdfc53aa80de774bf4ec62318
hab56ur9 <hab5bur9@gmail.com>
2024-10-24 23:43:37 +0900

    refactor(image): 사용하지 않는 create 로직 제거

--- src/domain/image/image.controller.ts
+++ src/domain/image/image.controller.ts
@@ -2,24 +2,17 @@ import {
   Controller,
   Get,
   Post,
-  Body,
   Param,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { ImageService } from './image.service';
-import { CreateImageDto } from './dto/create-image.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Express } from 'express';
 
 @Controller('image')
 export class ImageController {
   constructor(private readonly imageService: ImageService) {}
-
-  @Post()
-  create(@Body() createImageDto: CreateImageDto) {
-    return this.imageService.create(createImageDto);
-  }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
