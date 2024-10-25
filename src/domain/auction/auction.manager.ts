b624a6975817b46e9d1277d7f53e61ec6ff21b15
smart-cau <1995hyunwoo@naver.com>
2024-10-25 15:39:57 +0900

    feat(auction): Add ForbiddenBehaviorException to auction manager
    Introduces a new exception, ForbiddenBehaviorException, to handle cases where actions are restricted based on user authority. The auction manager uses this exception to ensure only owners can update or remove their auctions. Additionally, updated error codes to include FORBIDDEN_BEHAVIOR.

--- src/domain/auction/auction.manager.ts
+++ src/domain/auction/auction.manager.ts
@@ -3,6 +3,7 @@ import { CreateAuctionDto } from '~/src/domain/auction/dto/create-auction.dto';
 import { AuctionPolicy } from '~/src/domain/auction/auction.policy';
 import {
   EntityNotFoundException,
+  ForbiddenBehaviorException,
   TooEarlyEventTimeException,
 } from '~/src/common/exceptions/service.exception';
 import { MINUTE, SECOND } from '~/src/common/constants/time.constants';
@@ -11,6 +12,7 @@ import { Auction } from '~/src/domain/auction/entities/auction.entity';
 import { UpdateResult } from 'typeorm';
 import { User } from '~/src/domain/users/entities/user.entity';
 import { AuctionItem } from '~/src/domain/auction/entities/auction-item.entity';
+import { JwtPayloadDto } from '~/src/domain/auth/dto/jwt.dto';
 
 @Injectable()
 export class AuctionManager {
@@ -24,17 +26,23 @@ export class AuctionManager {
     if (!user) throw EntityNotFoundException(`user not found in auction`);
   }
 
-  public validateAuctionItem(auctionItem: AuctionItem) {
-    if (!auctionItem)
-      throw EntityNotFoundException(`auctionItem not found in auction`);
-  }
-
-  public validateId(id: string, auction: Auction) {
-    if (!auction) throw EntityNotFoundException(`auction id ${id} not found`);
+  public validateId(entity: Auction | AuctionItem) {
+    if (!entity) throw EntityNotFoundException(`id ${entity.id} not found`);
   }
 
   public checkAffected(result: UpdateResult) {
     if (result.affected === 0) throw EntityNotFoundException();
+  }
+
+  public authorityCheck(
+    auctionEntity: Auction | AuctionItem,
+    clientUser: JwtPayloadDto,
+  ) {
+    if (auctionEntity.user.id !== clientUser.id) {
+      throw ForbiddenBehaviorException(
+        'user can only remove or update their auction',
+      );
+    }
   }
 
   private isEventTimeTooEarly(eventDate: Date): boolean {
