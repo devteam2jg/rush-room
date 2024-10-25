f46a7c8cbbaf6f165ad0c611e9e7c8e7a71c9f75
hab56ur9 <hab5bur9@gmail.com>
2024-10-25 16:00:28 +0900

    feat(file): file service 환경 변수 설정
    - 환경 변수 및 S3client 연결

--- src/domain/file/file.service.ts
+++ src/domain/file/file.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
-
+import { S3Client } from '@aws-sdk/client-s3';
+import { ConfigService } from '@nestjs/config';
+import { MissingConfigurationsException } from '~/src/common/exceptions/service.exception';
 @Injectable()
-export class FileService {}
+export class FileService {
+  private readonly client: S3Client;
+  private readonly image_key: string;
+  private readonly image_secret: string;
+  private readonly image_bucket: string;
+  private readonly image_region: string;
+  constructor(private configService: ConfigService) {
+    this.image_bucket = configService.get<string>('S3_IMAGE_BUCKET_NAME');
+    this.image_key = configService.get<string>('S3_IMAGE_ACCESS_KEY');
+    this.image_secret = configService.get<string>('S3_IMAGE_ACCESS_SECRET');
+    this.image_region = configService.get<string>('S3_REGION');
+    this.checkConfigurations();
+    this.client = new S3Client({
+      region: this.image_region,
+      credentials: {
+        accessKeyId: this.image_key,
+        secretAccessKey: this.image_secret,
+      },
+    });
+  }
+  private checkConfigurations() {
+    if (
+      !this.image_bucket ||
+      !this.image_key ||
+      !this.image_secret ||
+      !this.image_region
+    )
+      throw MissingConfigurationsException('Missing S3 Configurations');
+  }
+}
