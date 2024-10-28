f3b556dee001926f1b6d72f9276577475b5e36cf
hab56ur9 <hab5bur9@gmail.com>
2024-10-28 13:53:24 +0900

    refactor(file): 변수 이름 변경

--- src/domain/file/file.service.ts
+++ src/domain/file/file.service.ts
@@ -6,33 +6,28 @@ import { MissingConfigurationsException } from '~/src/common/exceptions/service.
 @Injectable()
 export class FileService {
   private readonly client: S3Client;
-  private readonly image_key: string;
-  private readonly image_secret: string;
-  private readonly image_bucket: string;
-  private readonly image_region: string;
+  private readonly s3_key: string;
+  private readonly s3_secret: string;
+  private readonly s3_bucket: string;
+  private readonly s3_region: string;
 
   constructor(private readonly configService: ConfigService) {
-    this.image_bucket = configService.get<string>('S3_IMAGE_BUCKET_NAME');
-    this.image_key = configService.get<string>('S3_IMAGE_ACCESS_KEY');
-    this.image_secret = configService.get<string>('S3_IMAGE_ACCESS_SECRET');
-    this.image_region = configService.get<string>('S3_REGION');
+    this.s3_bucket = configService.get<string>('S3_IMAGE_BUCKET_NAME');
+    this.s3_key = configService.get<string>('S3_IMAGE_ACCESS_KEY');
+    this.s3_secret = configService.get<string>('S3_IMAGE_ACCESS_SECRET');
+    this.s3_region = configService.get<string>('S3_REGION');
     this.checkConfigurations();
     this.client = new S3Client({
-      region: this.image_region,
+      region: this.s3_region,
       credentials: {
-        accessKeyId: this.image_key,
-        secretAccessKey: this.image_secret,
+        accessKeyId: this.s3_key,
+        secretAccessKey: this.s3_secret,
       },
     });
   }
 
   private checkConfigurations() {
-    if (
-      !this.image_bucket ||
-      !this.image_key ||
-      !this.image_secret ||
-      !this.image_region
-    )
+    if (!this.s3_bucket || !this.s3_key || !this.s3_secret || !this.s3_region)
       throw MissingConfigurationsException('Missing S3 Configurations');
   }
 
@@ -41,7 +36,7 @@ export class FileService {
     const params = {
       Key: key,
       Body: file.buffer,
-      Bucket: this.image_bucket,
+      Bucket: this.s3_bucket,
       ContentType: file.mimetype,
     };
     const command = new PutObjectCommand(params);
@@ -51,7 +46,7 @@ export class FileService {
     return this.makeS3Url(key);
   }
   private makeS3Url(key: string) {
-    return `https://${this.image_bucket}.s3.${this.image_region}.amazonaws.com/${key}`;
+    return `https://${this.s3_bucket}.s3.${this.s3_region}.amazonaws.com/${key}`;
   }
 
   uploadImage(file: Express.Multer.File) {
