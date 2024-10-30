db795df98b17076494ecaebd9d7a8d49b62335f0
YJCMS <sen9563@gmail.com>
2024-10-30 16:48:38 +0900

    fix(auction-process): dependency injection 추가 및 수정

--- src/gateway/gateway.module.ts
+++ src/gateway/gateway.module.ts
@@ -1,7 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AuctionGateway } from '~/src/gateway/gateway';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuctionGateway } from './gateway';
+import { AuctionGatewayService } from './gateway.service';
+import { AuctionService } from '~/src/domain/auction/auction.service';
+import { AuctionManager } from '~/src/domain/auction/auction.manager';
+import { AuctionItemRepository } from '~/src/domain/auction/auction-item.repository';
+import { Auction } from '~/src/domain/auction/entities/auction.entity';
+import { AuctionItem } from '~/src/domain/auction/entities/auction-item.entity';
+import { User } from '~/src/domain/users/entities/user.entity';
+import { AuctionModule } from '~/src/domain/auction/auction.module';
+import { UsersModule } from '~/src/domain/users/users.module';
 
 @Module({
-  providers: [AuctionGateway],
+  imports: [
+    TypeOrmModule.forFeature([Auction, AuctionItem, User]),
+    AuctionModule,
+    UsersModule,
+  ],
+  providers: [
+    AuctionGateway,
+    AuctionGatewayService,
+    AuctionService,
+    AuctionManager,
+    AuctionItemRepository,
+  ],
+  exports: [AuctionGatewayService],
 })
 export class GatewayModule {}
