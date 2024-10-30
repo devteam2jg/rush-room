3e7ba607de747a148cf77a28df5d282345a74078
YJCMS <sen9563@gmail.com>
2024-10-30 20:34:44 +0900

    fix(auction-process): 게이트웨이 메세지 핸들러 args 타입 수정

--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -45,21 +45,21 @@ export class AuctionGateway {
     socket.join(auctionId);
     console.log(`Client ${socket.id} joined auction ${auctionId}`);
 
-    // 1. auctionId와 aucionItemId를 이용해 물품의 startPrice를 가져외서 currentBids에 저장
-    const auctionItem = await this.auctionService.getAuctionItem(
-      auctionId,
-      auctionItemId,
-    );
-    this.currentBids[auctionId] = auctionItem.startPrice;
-    // 2. 경매시간이 지난 후 currentBids[auctionId]를 lastPrice로 업데이트
+    // // 1. auctionId와 aucionItemId를 이용해 물품의 startPrice를 가져외서 currentBids에 저장
+    // const auctionItem = await this.auctionService.getAuctionItem(
+    //   auctionId,
+    //   auctionItemId,
+    // );
+    // this.currentBids[auctionId] = auctionItem.startPrice;
+    // // 2. 경매시간이 지난 후 currentBids[auctionId]를 lastPrice로 업데이트
 
     if (this.currentBids[auctionId] == null) {
-      this.currentBids[auctionId] = 9999999999;
+      this.currentBids[auctionId] = 9999;
     }
 
     const currentBid = this.currentBids[auctionId];
     socket.emit('current_bid', currentBid);
-    socket.emit('item_info', auctionItem); //
+    // socket.emit('item_info', auctionItem);
   }
 
   /**
@@ -115,7 +115,12 @@ export class AuctionGateway {
   @SubscribeMessage('message')
   handleMessage(
     socket: Socket,
-    messageData: { auctionId: string; userId: string; message: string, nickname: string },
+    messageData: {
+      auctionId: string;
+      userId: string;
+      message: string;
+      nickname: string;
+    },
   ): void {
     const { auctionId } = messageData;
     this.server.to(auctionId).emit('message', messageData);
