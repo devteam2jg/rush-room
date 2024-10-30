dffd6cfc0734f97223ecadfadd46de06e2bda13d
hab56ur9 <hab5bur9@gmail.com>
2024-10-30 16:51:46 +0900

    feat(aws): dto추가 , validator 추가
    - aws dto 추가 :  환경변수를 저장할 provider가 validation을 검증하기 위해 사용
    - validate-env.ts 추가 : 클래스 validation을 검증하는 export function

---
+++ src/domain/aws/dto/aws.dto.ts
@@ -0,0 +1,39 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class AwsUserConfigDto {
+  @IsNotEmpty()
+  @IsString()
+  accountId: string;
+  @IsNotEmpty()
+  @IsString()
+  region: string;
+  @IsNotEmpty()
+  @IsString()
+  accessKeyId: string;
+  @IsNotEmpty()
+  @IsString()
+  secretAccessKey: string;
+}
+
+export class AwsFileConfigDto {
+  @IsNotEmpty()
+  @IsString()
+  bucket: string;
+}
+export class AwsMediaConvertConfigDto {
+  @IsNotEmpty()
+  @IsString()
+  bucket: string;
+  @IsNotEmpty()
+  @IsString()
+  endpoint: string;
+}
+
+export class AwsConfigDto {
+  @IsNotEmpty()
+  user: AwsUserConfigDto;
+  @IsNotEmpty()
+  file: AwsFileConfigDto;
+  @IsNotEmpty()
+  mediaConvert: AwsMediaConvertConfigDto;
+}
