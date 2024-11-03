827a7f3076e5fec4eb72656b172b4c5f30781891
YJCMS <sen9563@gmail.com>
2024-11-03 16:45:14 +0900

    feat(game-service): 경매 game service context 생성 기능

--- src/domain/game/game.service.ts
+++ src/domain/game/game.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { AuctionGameContext, BidItem } from './game.context';
+import { AuctionService } from '~/src/domain/auction/auction.service';
 
 @Injectable()
-export class GameService {}
+export class GameService {
+  constructor(private readonly auctionService: AuctionService) {}
+
+  private auctionsMap: Map<string, AuctionGameContext> = new Map();
+
+  // 경매 정보를 가져와서 경매 게임 컨텍스트를 생성
+  async createGameContext(auctionId: string, auctionitemId) {
+    const auction = await this.auctionService.getAuctionDetail(
+      auctionId,
+      auctionitemId,
+    );
+    const bidItems: BidItem[] = auction.items.map((item) => ({
+      itemId: item.id,
+      sellerId: item.postedUser,
+      startPrice: item.startPrice,
+      bidPrice: item.startPrice,
+      bidderId: '',
+      itemSellingLimitTime: auction.auctionDto.sellingLimitTime,
+      title: item.title,
+      description: item.description,
+      picture: item.imageUrls,
+    }));
+    const auctionGameContext = new AuctionGameContext(
+      auctionId,
+      auction.auctionDto.eventDate,
+      bidItems,
+    );
+    this.auctionsMap.set(auctionId, auctionGameContext);
+    return auctionGameContext;
+  }
+
+  joinAuction(auctionId) {
+    const auction = this.auctionsMap.get(auctionId);
+    if (!auction) {
+      throw new Error('Auction not found');
+    }
+    return auction;
+  }
+}
