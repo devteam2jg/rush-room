410f86e743db885fd54118a29f375b641ce32abe
YJCMS <sen9563@gmail.com>
2024-11-03 16:44:41 +0900

    fix(game-service): 경매 game context 타입 수정

--- src/domain/game/game.context.ts
+++ src/domain/game/game.context.ts
@@ -1,3 +1,5 @@
+import { UserProfileDto } from '../users/dto/user.dto';
+
 export enum AuctionStatus {
   READY = 'READY',
   ONGOING = 'ONGOING',
@@ -5,24 +7,24 @@ export enum AuctionStatus {
 }
 export class BidItem {
   itemId: string;
-  sellerId: string;
+  sellerId: UserProfileDto;
   bidderId: string;
   startPrice: number;
   bidPrice: number;
   itemSellingLimitTime: number;
   title: string;
   description: string;
-  picture: string;
+  picture: string[];
 }
 export class AuctionGameContext {
   auctionId: string;
   bidItems: BidItem[];
-  auctionStartDateTime: number;
+  auctionStartDateTime: Date;
   private auctionStatus: AuctionStatus;
   private currentBidItem: BidItem;
   constructor(
     auctionId: string,
-    auctionStartDateTime: number,
+    auctionStartDateTime: Date,
     bidItems: BidItem[],
   ) {
     this.auctionId = auctionId;
