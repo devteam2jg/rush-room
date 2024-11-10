96791a28fe6800b24d593b2d76a24e8e9ce25e3e
hab56ur9 <hab5bur9@gmail.com>
2024-11-11 02:30:38 +0900

    feat(game): 게임 테스트 컨트롤러 추가

--- src/domain/game/game.module.ts
+++ src/domain/game/game.module.ts
@@ -7,11 +7,12 @@ import { UsersModule } from '~/src/domain/users/users.module';
 
 import { GameGuard } from '~/src/domain/game/guards/game.guard';
 import { GameStatusService } from '~/src/domain/game/services/game.status.service';
+import { GameTestController } from '~/src/domain/game/test/game-test.controller';
 
 @Module({
   imports: [AuctionModule, UsersModule],
   providers: [GameGateway, GameService, GameGuard, GameStatusService],
   exports: [GameService],
-  controllers: [GameController],
+  controllers: [GameController, GameTestController],
 })
 export class GameModule {}
