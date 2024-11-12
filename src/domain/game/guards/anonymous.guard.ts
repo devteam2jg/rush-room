e8c7b49b6c96733ec432ce8dbc0421f19cf23f49
hab56ur9 <hab5bur9@gmail.com>
2024-11-12 22:21:03 +0900

    fix(game): Guard 로직 수정

--- src/domain/game/guards/anonymous.guard.ts
+++ src/domain/game/guards/anonymous.guard.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { UpdateBidPriceDto } from '~/src/domain/game/dto/game.dto';
 import { GameStatusService } from '~/src/domain/game/services/game.status.service';
 import { SocialType } from '~/src/domain/users/enum/social-type.enum';
 @Injectable()
@@ -6,8 +7,8 @@ export class AnonymousGuard {
   constructor(private readonly gameStatusService: GameStatusService) {}
   canActivate(context) {
     const client = context.switchToWs().getClient();
-    const data = context.switchToWs().getData();
-    const { auctionId, userId } = data.auctionId;
+    const data: UpdateBidPriceDto = context.switchToWs().getData();
+    const { auctionId, userId } = data;
     if (!auctionId) {
       client.emit('ERROR', 'Auction ID is required');
       return false;
