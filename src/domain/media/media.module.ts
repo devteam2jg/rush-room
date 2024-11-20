f4ba0b3aacccb2b73110fdabf6380a68a455462b
hab56ur9 <hab5bur9@gmail.com>
2024-11-20 22:17:51 +0900

    chore(bid)

--- src/domain/media/media.module.ts
+++ src/domain/media/media.module.ts
@@ -1,16 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MediasoupModule } from './mediasoup/mediasoup.module';
 import { SignalingModule } from './signaling/signaling.module';
-import { BullModule } from '@nestjs/bull';
 import { MediaProcessor } from '~/src/domain/media/queue/media.processor';
 @Module({
-  imports: [
-    BullModule.registerQueue({
-      name: 'media-queue',
-    }),
-    MediasoupModule,
-    SignalingModule,
-  ],
+  imports: [MediasoupModule, SignalingModule],
   providers: [MediaProcessor],
 })
 export class MediaModule {}
