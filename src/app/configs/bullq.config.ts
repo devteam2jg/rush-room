6e41ecad342822c35c1b97f166dc601f385db5d2
hab56ur9 <hab5bur9@gmail.com>
2024-11-20 23:46:59 +0900

    chore

--- src/app/configs/bullq.config.ts
+++ src/app/configs/bullq.config.ts
@@ -1,8 +1,8 @@
 import { ConfigService } from '@nestjs/config';
 export const bullqConfig = (configService: ConfigService) => ({
   redis: {
-    host: configService.get<string>('REDIS_HOST', 'localhost'),
-    port: configService.get<number>('REDIS_PORT', 6379),
+    host: configService.get<string>('REDIS_HOST'),
+    port: configService.get<number>('REDIS_PORT'),
     password: configService.get<string>('REDIS_PASSWORD'),
   },
 });
